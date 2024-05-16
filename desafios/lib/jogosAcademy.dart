c1d2c4bbd0a6ff3fd6844c6abb35d1c7b18f1a35
Alexandre <xande.haus@gmail.com>
2024-05-16 14:23:14 -0300

    adicionado comentario doque falta fazer

--- desafios/lib/jogosAcademy.dart
+++ desafios/lib/jogosAcademy.dart
@@ -634,4 +634,5 @@ class ThemeProvider with ChangeNotifier {
 //corrigir bugs (jogo da velha quando aparece a notificação de vencedor ou empate se clicado fora buga a aplicação)
 //melhorar o visual da tela de botões 
 //
-//
+//fazer um banco de dados para guardar algumas coisas 
+//se sobrar tempo acrecentar um jogo de caça palavras e alguns outros joguinhos
